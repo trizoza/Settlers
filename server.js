87a21678eac104fdb18a120f0969d075a3b610e6
Peter Trizuliak <peter.trizuliak@gmail.com>
2017-08-06 16:25:40 +0100

    fix app.listen to PORT and 5000

--- server.js
+++ server.js
@@ -1,37 +1,43 @@
 var express = require('express')
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
 
-// http.listen(app.get('port')); 
+// http.listen(app.get('port'));
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static('client/build'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/client/build/index.html');
 });
 
-io.on('connection', function(socket) {
-  console.log('a user connected');
-
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-
-  socket.on('game-event', function(data) {
-    console.log("on game event in server.js")
-    io.emit('receive data', data)
-  })
-
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
-http.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port HEROKU mode");
-});
+
+// io.on('connection', function(socket) {
+//   console.log('a user connected');
+
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+//   });
+
+//   socket.on('game-event', function(data) {
+//     console.log("on game event in server.js")
+//     io.emit('receive data', data)
+//   })
+
+// });
+
 
 // http.listen(3000, function(){
 //   console.log('listening on *:3000');
 // });
 
-app.use(express.static('client/build'));
+
 
 
 // var server = app.listen(3000, function () {
